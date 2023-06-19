91bc09a2f8a5f881a807c1635533708d7d232eb5
shanguanma <nanr9544@gmail.com>
2023-06-19 17:13:24 +0800

    add using fairseqlm decoding example

--- examples/imls_ssl/base_imls-ssl.sh
+++ examples/imls_ssl/base_imls-ssl.sh
@@ -8,21 +8,29 @@ stop_stage=1000
 
 export HYDRA_FULL_ERROR=1
 export CUDA_LAUNCH_BLOCKING=1
-## (TODO) first run it
+
+## build c++ part using CUDA, however in head node of this slurm server system hasn't cuda gpu.
+cd /mntnfs/lee_data1/maduo/codebase/fairseq_speechtext
+pip install ninja ## using fast  distutils backend training for pytorch, it is very important
+pip install --editable ./  ## for python package, it can be installed at local environment
+python setup.py build_ext --inplace
+
+
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ];then
    echo "iter: pretrain imls_ssl on 6layer of hubert pesudo label and librispeech monophncode from w2vu2-model " 
    echo "training on 400k steps for train-960 of librispeech"
-   fairseq_dir=/workspace2/maduo/fairseq_speechtext
-   tsv_dir=/workspace2/maduo/dataset/format/librispeech
+   fairseq_dir=/mntnfs/lee_data1/maduo/codebase/fairseq_speechtext
+   tsv_dir=/mntcephfs/lab_data/maduo/datasets/format/librispeech/
+   dir=/mntnfs/lee_data1/maduo/exp
    label_dir=$tsv_dir/offical_hubert_codes_and_librispeech_frame_monophncode_using_wav2vec-u2_model
    config_dir=$fairseq_dir/examples/imls_ssl
-   dir=/workspace2/maduo/exp
    model_name=pretrain_on_base_imls-ssl_4gpu_8update_960h_400k_update
    exp_dir=$dir/pretrain/${model_name}
    mkdir -p $exp_dir
    world_size=4
    update_freq=8
-   CUDA_VISIBLE_DEVICES=4,5,6,7 python $fairseq_dir/fairseq_cli/hydra_train.py \
+   export PYTHONPATH=$fairseq_dir:$PYTHONPATH
+   python $fairseq_dir/fairseq_cli/hydra_train.py \
             --config-dir $config_dir/config/pretrain \
             --config-name imls_ssl_base_librispeech \
             task.data=$tsv_dir\
@@ -40,31 +48,32 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ];then
             checkpoint.save_dir=$exp_dir\
             hydra.run.dir=$fairseq_dir/examples/imls_ssl\
             hydra.job.name=$exp_dir/pretrain
-### 4RTX3090: training about  day
-###           400steps: about  minites
+### 4V100: training about 30.6 day
+###           200steps: about 22 minites
 fi
 
 
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ];then
    echo "fine tune base imls-ssl model  using train-clean-100 supervision data"
-   fairseq_dir=/workspace2/maduo/fairseq_speechtext
-   tsv_dir=/workspace2/maduo/dataset/format/librispeech
+   fairseq_dir=/mntnfs/lee_data1/maduo/codebase/fairseq_speechtext
+   tsv_dir=/mntcephfs/lab_data/maduo/datasets/format/librispeech/
+   dir=/mntnfs/lee_data1/maduo/exp
    config_dir=$fairseq_dir/examples/imls_ssl
-   dir=/workspace2/maduo/exp
    model_name=pretrain_on_base_imls-ssl_4gpu_8update_960h_400k_update
    exp_finetune_dir=$dir/finetune/${model_name}_100h_asr_finetune
    exp_dir=$dir/pretrain/${model_name}
    mkdir -p $exp_finetune_dir
    world_size=4
    update_freq=2
-   CUDA_VISIBLE_DEVICES=4,5,6,7 python $fairseq_dir/fairseq_cli/hydra_train.py \
+   export PYTHONPATH=$fairseq_dir:$PYTHONPATH
+   python $fairseq_dir/fairseq_cli/hydra_train.py \
        --config-dir $config_dir/config/finetune \
             --config-name imls_ssl_base_100h \
             task.data=$tsv_dir\
             task.label_dir=$tsv_dir\
             task.labels='["ltr"]' \
-            model.w2v_path=$exp_dir/checkpoint_last.pt\
+            model.w2v_path=$exp_dir/checkpoint_298_400000.pt\
             common.user_dir=$fairseq_dir/examples/imls_ssl\
             dataset.train_subset=train-clean-100\
             dataset.valid_subset=dev-other\
@@ -80,32 +89,125 @@ fi
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ];then
    echo "inference imls-ssl  model on dev-other, dev-clean, test-other, test-clean of librispeech"
-   fairseq_dir=/workspace2/maduo/fairseq_speechtext
-   tsv_dir=/workspace2/maduo/dataset/format/librispeech
+   fairseq_dir=/mntnfs/lee_data1/maduo/codebase/fairseq_speechtext
+   tsv_dir=/mntcephfs/lab_data/maduo/datasets/format/librispeech/
+   dir=/mntnfs/lee_data1/maduo/exp
+
    config_dir=$fairseq_dir/examples/hubert/
-   dir=/workspace2/maduo/exp
-   model_name=pretrain_on_base_imls-ssl_4gpu_8update_960h_ils-ssl_400k_update
+   model_name=pretrain_on_base_imls-ssl_4gpu_8update_960h_400k_update
    exp_finetune_dir=$dir/finetune/${model_name}_100h_asr_finetune
-   mkdir -p $exp_finetune_dir/decode_on_100h
+   #results_path=$exp_finetune_dir/decode_on_100h
+   results_path=$exp_finetune_dir/decode_on_100h_normalize_false
+   mkdir -p $results_path
    testsets="dev-clean dev-other test-clean test-other"
    export PYTHONPATH=$fairseq_dir:$PYTHONPATH
 
    for name in $testsets;do
        python $fairseq_dir/examples/speech_recognition/new/infer.py \
-                --config-dir $config_dir/config/decode\
-                --config-name infer_viterbi\
+                --config-dir $fairseq_dir/examples/speech_recognition/new/conf\
+                --config-name infer_viterbi_librispeech\
                 task.data=$tsv_dir\
                 task.label_dir=$tsv_dir\
-                task.normalize=true\
-                common_eval.results_path=$exp_finetune_dir/decode_on_100h\
+                task.normalize=false\
+                common_eval.results_path=$results_path\
                 common_eval.path=$exp_finetune_dir/checkpoint_best.pt\
                 dataset.gen_subset=$name
 
    done
-   # WER% is from $exp_finetune_dir/decode_on_100/viterbi/infer.log
-   #  dev-clean   dev-other   test-clean   test-other
+   # WER% is grep -rn "Word error rate" $exp_finetune_dir/decode_on_100/viterbi/infer.log
+   ## or  grep -rn "Word error rate" logs/base_imls-ssl_stage3_infer.log
+   #  dev-clean   dev-other   test-clean   test-other  ## task.normalize=true, however it is setting false at pretrain stage.
+   #    4.4373     10.6254     4.4420      10.1601
+   #  grep -rn "Word error rate" logs/base_imls-ssl_stage3_infer_normalize_false.log , ## task.normalize=false. it is same as pretain stage
+   #    4.4576     10.4095     4.4420      10.1677
 
 
 fi
 
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ];then
+   echo "inference imls-ssl  model on dev-other, dev-clean, test-other, test-clean of librispeech with kenlm"
+   fairseq_dir=/mntnfs/lee_data1/maduo/codebase/fairseq_speechtext
+   tsv_dir=/mntcephfs/lab_data/maduo/datasets/format/librispeech/
+   dir=/mntnfs/lee_data1/maduo/exp
+
+   config_dir=$fairseq_dir/examples/hubert/
+   model_name=pretrain_on_base_imls-ssl_4gpu_8update_960h_400k_update
+   exp_finetune_dir=$dir/finetune/${model_name}_100h_asr_finetune
+   results_path=$exp_finetune_dir/decode_on_100h_with_kenlm
+   mkdir -p $results_path
+   path_to_lexicon=/mntcephfs/lab_data/maduo/datasets/librispeech/kenlm_files/librispeech_lexicon.lst #word2letter
+   path_to_lm=/mntcephfs/lab_data/maduo/datasets/librispeech/kenlm_files/4-gram.arpa  ## word lm
+   testsets="dev-clean dev-other test-clean test-other"
+   export PYTHONPATH=$fairseq_dir:$PYTHONPATH
+
+   for name in $testsets;do
+       python $fairseq_dir/examples/speech_recognition/new/infer.py \
+                --config-dir $fairseq_dir/examples/speech_recognition/new/conf\
+                --config-name infer_kenlm_lirispeech\
+                task.data=$tsv_dir\
+                task.label_dir=$tsv_dir\
+                task.normalize=false\
+                common_eval.results_path=$results_path\
+                common_eval.path=$exp_finetune_dir/checkpoint_best.pt\
+                dataset.gen_subset=$name\
+                decoding.type=kenlm\
+                decoding.lexicon=$path_to_lexicon\
+                decoding.lmpath=$path_to_lm\
+                decoding.nbest=1\
+                decoding.beam=1500 \
+                decoding.lmweight=2 \
+                decoding.wordscore=-1 \
+                decoding.beamthreshold=100\
+                common_eval.quiet=false
+
+   done
+   # WER% in kenlm and task.normalize=false
+   # log is from grep -rn "Word error rate" logs/base_imls-ssl_stage4_infer_with_kenlm.log
+   # dev-clean  dev-other test-clean test-other
+   # 2.5459      7.2079      3.0666     7.2580
+   ## NOTE: for 1h / 10h ft model , you should set decoding.lmweight=3 to decode. will get best WER%
+ fi
+
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ];then
+   echo "inference imls-ssl  model on dev-other, dev-clean, test-other, test-clean of librispeech with fairseqlm"
+   fairseq_dir=/mntnfs/lee_data1/maduo/codebase/fairseq_speechtext
+   tsv_dir=/mntcephfs/lab_data/maduo/datasets/format/librispeech/
+   dir=/mntnfs/lee_data1/maduo/exp
+
+   config_dir=$fairseq_dir/examples/hubert/
+   model_name=pretrain_on_base_imls-ssl_4gpu_8update_960h_400k_update
+   exp_finetune_dir=$dir/finetune/${model_name}_100h_asr_finetune
+   results_path=$exp_finetune_dir/decode_on_100h_with_fairseqlm_normalize_false
+   mkdir -p $results_path
+   path_to_lexicon=/mntcephfs/lab_data/maduo/datasets/librispeech/kenlm_files/librispeech_lexicon.lst #word2letter, it is upper style dictionary
+   path_to_lm=/mntcephfs/lab_data/maduo/datasets/librispeech/fairseqlm_files/lm_librispeech_word_transformer.pt  ## word lm
+   #word_dict=/mntcephfs/lab_data/maduo/datasets/librispeech/fairseqlm_files/dict.txt ## lower style word dictionary, first column is word 
+                                                                                     ## second column is word frequence.
+   #testsets="dev-clean dev-other test-clean test-other"
+   testsets="dev-clean"
+   export PYTHONPATH=$fairseq_dir:$PYTHONPATH
+   for name in $testsets;do
+       python $fairseq_dir/examples/speech_recognition/new/infer.py \
+                --config-dir $fairseq_dir/examples/speech_recognition/new/conf\
+                --config-name infer_fsqlm_librispeech\
+                task.data=$tsv_dir\
+                task.label_dir=$tsv_dir\
+                task.normalize=false\
+                common_eval.results_path=$results_path\
+                common_eval.path=$exp_finetune_dir/checkpoint_best.pt\
+                dataset.gen_subset=$name\
+                decoding.type=fairseqlm\
+                decoding.lexicon=$path_to_lexicon\
+                decoding.lmpath=$path_to_lm\
+                decoding.nbest=1\
+                decoding.beam=500 \
+                decoding.lmweight=2 \
+                decoding.wordscore=-1 \
+                decoding.beamthreshold=25\
+                common_eval.quiet=false
+
+
+   done
+
+fi
 
