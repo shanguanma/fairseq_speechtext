9627dc5c453abc7b65c23a2b86bb5c5db37522e5
shanguanma <nanr9544@gmail.com>
2023-06-21 18:02:08 +0800

    update base_imls-ssl.sh

--- examples/imls_ssl/base_imls-ssl.sh
+++ examples/imls_ssl/base_imls-ssl.sh
@@ -183,8 +183,8 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ];then
    path_to_lm=/mntcephfs/lab_data/maduo/datasets/librispeech/fairseqlm_files/lm_librispeech_word_transformer.pt  ## word lm
    #word_dict=/mntcephfs/lab_data/maduo/datasets/librispeech/fairseqlm_files/dict.txt ## lower style word dictionary, first column is word 
                                                                                      ## second column is word frequence.
-   #testsets="dev-clean dev-other test-clean test-other"
-   testsets="dev-clean"
+   testsets="dev-other test-clean test-other"
+   #testsets="dev-clean"
    export PYTHONPATH=$fairseq_dir:$PYTHONPATH
    for name in $testsets;do
        python $fairseq_dir/examples/speech_recognition/new/infer.py \
@@ -208,6 +208,57 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ];then
 
 
    done
+   ##WER% with fairseqlm(it is a transformer network lm) to decode
+   ## dev-clean  dev-other test-clean test-other   lmweight=2
+   ## 2.4448      6.3089    2.6861      6.3696 
+   
+fi
+
+if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ];then
+   echo "inference imls-ssl  model on dev-other, dev-clean, test-other, test-clean of librispeech with fairseqlm"
+   fairseq_dir=/mntnfs/lee_data1/maduo/codebase/fairseq_speechtext
+   tsv_dir=/mntcephfs/lab_data/maduo/datasets/format/librispeech/
+   dir=/mntnfs/lee_data1/maduo/exp
+
+   config_dir=$fairseq_dir/examples/hubert/
+   model_name=pretrain_on_base_imls-ssl_4gpu_8update_960h_400k_update
+   exp_finetune_dir=$dir/finetune/${model_name}_100h_asr_finetune
+   results_path=$exp_finetune_dir/decode_on_100h_with_fairseqlm_normalize_false
+   mkdir -p $results_path
+   path_to_lexicon=/mntcephfs/lab_data/maduo/datasets/librispeech/kenlm_files/librispeech_lexicon.lst #word2letter, it is upper style dictionary
+   path_to_lm=/mntcephfs/lab_data/maduo/datasets/librispeech/fairseqlm_files/lm_librispeech_word_transformer.pt  ## word lm
+   #word_dict=/mntcephfs/lab_data/maduo/datasets/librispeech/fairseqlm_files/dict.txt ## lower style word dictionary, first column is word
+                                                                                     ## second column is word frequence.
+   testsets="dev-clean dev-other test-clean test-other"
+   #testsets="dev-clean"
+   export PYTHONPATH=$fairseq_dir:$PYTHONPATH
+   for name in $testsets;do
+       python $fairseq_dir/examples/speech_recognition/new/infer.py \
+                --config-dir $fairseq_dir/examples/speech_recognition/new/conf\
+                --config-name infer_fsqlm_librispeech\
+                task.data=$tsv_dir\
+                task.label_dir=$tsv_dir\
+                task.normalize=false\
+                common_eval.results_path=$results_path\
+                common_eval.path=$exp_finetune_dir/checkpoint_best.pt\
+                dataset.gen_subset=$name\
+                decoding.type=fairseqlm\
+                decoding.lexicon=$path_to_lexicon\
+                decoding.lmpath=$path_to_lm\
+                decoding.nbest=1\
+                decoding.beam=500 \
+                decoding.lmweight=3 \
+                decoding.wordscore=-1 \
+                decoding.beamthreshold=25\
+                common_eval.quiet=false
+
+
+   done
+   ##WER% with fairseqlm(it is a transformer network lm) to decode
+   ## dev-clean  dev-other  test-clean  test-other   lmweight=3
+   ##  grep -rn "Word error rate:" logs/base_imls-ssl_stage6_infer_with_fairseqlm_normalize_false_lmweight3.log
+   ##   3.2352    8.1187     3.3538    8.1732
 
 fi
+
 
