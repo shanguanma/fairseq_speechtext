c9c7e8869b580f6aab96128e85ef86b30c57219a
shanguanma <maduo25@163.com>
2023-09-27 10:13:24 +0800

    add new config

--- examples/voicelm/base_voicelm_local_cluster.sh
+++ examples/voicelm/base_voicelm_local_cluster.sh
@@ -4,8 +4,8 @@ stage=0
 
 stop_stage=1000
 .  utils/parse_options.sh
-. path_for_fairseq_speechtext.sh ## pytorch 2.0 fairseq flashnight-text flashnight sequence 
-
+#. path_for_fairseq_speechtext.sh ## pytorch 2.0 fairseq flashnight-text flashnight sequence 
+. path_for_fsq_speechtext.sh
 export HYDRA_FULL_ERROR=1
 export CUDA_LAUNCH_BLOCKING=1
 
